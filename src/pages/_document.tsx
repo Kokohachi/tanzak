1e72d7ce441d41ca834081e46aee8ab27049b63e
Kokohachi <99192596+Kokohachi@users.noreply.github.com>
2024-07-07 14:27:39 +0900

    Fix: responsive&og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,24 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <ColorSchemeScript defaultColorScheme="dark" />
+        <title>tanzak</title>
+        <meta name="description" content="tanzakで短冊を投げよう" />
+        {/* make it 50% smaller on smartphone */}
+        <meta name="viewport" content="width=device-width, initial-scale=0.5" />
+        {/* og image */}
+        <meta property="og:image" content="https://tanzak.vercel.app/ogimage.png" />
+        <meta property="og:title" content="tanzak" />
+        <meta property="og:description" content="tanzakで短冊を投げよう" />
+        <meta property="og:url" content="https://tanzak.vercel.app" />
+        <meta property="og:type" content="website" />
+        {/* twitter card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@eutrilo" />
+        <meta name="twitter:creator" content="@eutrilo" />
+        <meta name="twitter:title" content="tanzak" />
+        <meta name="twitter:description" content="tanzakで短冊を投げよう" />
+        <meta name="twitter:image" content="https://tanzak.vercel.app/ogimage.png" />
+        
       </Head>
 
       <body>
